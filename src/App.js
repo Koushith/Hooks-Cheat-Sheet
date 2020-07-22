af0d2748b231d854fb6739538387ec8a6c507ffc
koushith <koushith@gmail.com>
2020-07-23 00:46:25 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ResourceList from './ResourceList';
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts',
-  };
+// class App extends React.Component {
+//   state = {
+//     resourceName: 'posts',
+//   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>
-          Posts
-        </button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>
-          Todos
-        </button>
-        <ResourceList resourceName={this.state.resourceName} />
-      </React.Fragment>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <button onClick={() => this.setState({ resourceName: 'posts' })}>
+//           Posts
+//         </button>
+//         <button onClick={() => this.setState({ resourceName: 'todos' })}>
+//           Todos
+//         </button>
+//         <ResourceList resourceName={this.state.resourceName} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [resource, setRecource] = useState('posts');
+
+  <React.Fragment>
+    <button onClick={() => setRecource('posts')}>Posts</button>
+    <button onClick={() => setRecource('todos')}>Todos</button>
+    <ResourceList resourceName={this.state.resourceName} />
+  </React.Fragment>;
+};
 export default App;
